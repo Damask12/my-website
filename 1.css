29e9c37c94a30a3944fbeeac5053dc018d4cfe2c
Bulat Zhambalov <bulatv888@gmail.com>
2024-12-10 13:15:10 +0700

    Create 1.css

---
+++ 1.css
@@ -0,0 +1,216 @@
+* {
+  box-sizing: border-box;
+  
+}
+
+.main{
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    width: 555px; 
+    height: 812px; 
+    top: 15px;
+    left: 20px;
+    gap: 20px; 
+  border: 1px solid #000; 
+  color: rgb(255,255,255,1);
+}
+
+.head {
+  gap: 20px;
+  display: flex;
+}
+
+.Avatar {
+  width: 124px;
+  height: 124px;
+  border-radius: 12px;
+}
+
+.nameBox {
+  background-color: #FF2400;
+  width: 155px;
+  height: 124px;
+  padding: 12px;
+  border-radius: 12px;
+}
+
+.greetingPhrase {
+  width: 100px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 15px;
+  color: rgb(255,255,255,1);;
+  text-align: center;
+  font-weight: bold;
+}
+
+.name {
+  width: 100px;
+  height: 50px;
+  font-family: Poppins;
+  font-size: 17px;
+  text-align: center;
+  color: rgb(255,255,255,1);
+}
+
+.role {
+  width: 100px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 11px;
+  text-align: center;
+  color: rgb(255,255,255,1);
+}
+
+.languagesBox {
+  background-color: #FF2400;
+  width: 221px;
+  height: 124px;
+  padding: 12px;
+  border-radius: 12px;
+}
+
+.title {
+  height: 21px;
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.english {
+  height: 20px;
+  font-family: Poppins;
+  font-size: 15px;
+  display: flex;
+}
+
+.engflag {
+  width: 24px;
+  height: 14px;
+  border-radius: 12px;
+}
+
+.russian {
+  height: 20px;
+  font-family: Poppins;
+  font-size: 15px;
+  display: flex;
+}
+
+.rusflag {
+  width: 24px;
+  height: 14px;
+  border-radius: 12px;
+}
+
+.german {
+  height: 20px;
+  font-family: Poppins;
+  font-size: 15px;
+  display: flex;
+}
+
+.gerflag {
+  width: 24px;
+  height: 14px;
+  border-radius: 12px;
+}
+
+.body {
+  display: flex;
+  gap: 16px;
+}
+.expBox {
+  background-color: #FF2400;
+  width: 300px;
+  height: 300px;
+  padding: 12px;
+  border-radius: 12px;
+}
+
+.exp {
+  height: 250px;
+  font-family: Poppins;
+  font-size: 15px;
+  display: flex;
+  text-align: center;
+}
+.toolsBox {
+  background-color: #FF2400;
+  width: 225px;
+  height: 300px;
+  border-radius: 12px;
+}
+
+.tools {
+  height: 20px;
+  font-family: Poppins;
+  font-size: 15px;
+  display: flex;
+}
+.foot {
+  gap: 20px;
+  display: flex;
+}
+.educationBox {
+  background-color: #FF2400;
+  width: 300px;
+  height: 300px;
+  padding: 12px;
+  border-radius: 12px;
+}
+.interestsBox {
+  background-color: #FF2400;
+  width: 220px;
+  height: 170px;
+  padding: 12px;
+  border-radius: 12px;
+  margin-bottom: 12px;
+}
+.schoolBox {
+  background-color: #FF2400;
+  width: 195px;
+  height: 100px;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 12px;
+}
+.contactBox {
+  background-color: #FF2400;
+  width: 220px;
+  height: 120px;
+  padding: 12px;
+  border-radius: 12px;
+}
+.contactText {
+  font-size: 14px;
+  font-family: Poppins;
+  font-weight: bold;
+  text-align: center;
+}
+.PDFbutton {
+  margin-left: auto;
+  margin-right: auto;
+}
+.button {
+  background-color: #FF2400;
+  width: 220px;
+  height: 60px;
+  border-radius: 12px;
+}
+.PDFtext {
+  font-size: 20px;
+  font-family: Poppins;
+  font-weight: bold;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+}
+.saveButton{
+    font-weight: bold;
+    width: 200px;
+    margin-left: auto;
+    margin-right: auto;
+}
